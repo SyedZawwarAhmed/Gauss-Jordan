be7444d50d43138497c3890fc85637f9dbceb2d7
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-05-17 06:49:42 +0500

    changes made

--- GaussJordan.py
+++ GaussJordan.py
@@ -8,8 +8,11 @@ main_list[1][0] = float(input("Enter co-efficient of x:- "))
 main_list[1][1] = float(input("Enter co-efficient of y:- "))
 main_list[1][2] = float(input("Enter constant:- "))
 
-for i in range(len(main_list)): 
-    multiplicative_inverse = 1/main_list[i][i]
+for i in range(len(main_list)):
+    if main_list[i][i] != 0:
+        multiplicative_inverse = 1/main_list[i][i]
+    else:
+        multiplicative_inverse = 0
     for j in range(len(main_list[i])):
         main_list[i][j] *= multiplicative_inverse
 
