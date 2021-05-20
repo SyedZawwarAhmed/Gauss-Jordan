ca705f0a0b4ab2fd122ef8c20ed6b11fbf38bf95
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-05-20 23:09:18 +0500

    changes made

--- GaussJordan.py
+++ GaussJordan.py
@@ -19,10 +19,10 @@ for i in range(len(main_list)):
     if i == 0:
         additive_inverse = main_list[i+1][i] * -1
         for k in range(len(main_list[i+1])):
-            main_list[i+1][k] = main_list[i+1][k] + (main_list[i][k] * additive_inverse)
+            main_list[i+1][k] += (main_list[i][k] * additive_inverse)
     if i == 1:
         additive_inverse = main_list[i-1][i] * -1
         for k in range(len(main_list[i-1])):
-            main_list[i-1][k] = main_list[i-1][k] + (main_list[i][k] * additive_inverse)
+            main_list[i-1][k] += (main_list[i][k] * additive_inverse)
 
 print(" x = ", main_list[0][-1], "\n", "y = ", main_list[1][-1])
